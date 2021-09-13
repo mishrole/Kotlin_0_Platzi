f2490929ae70e467cc65158b8946cd329a06cf4b
Mitchell Rodríguez <mitrodle@gmail.com>
2021-09-12 20:39:39 -0500

    add: Project Random Response

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
+/*
+* Bug windows with intellij terminal
+*   enter
+* */
+
 //Declarando valores de las respuestas
 const val AFFIRMATIVE_ANSWER = "✅"
 const val NEGATIVE_ANSWER = "❌"
@@ -18,7 +23,6 @@ val answers = mapOf(
     "I don't believe it" to NEGATIVE_ANSWER
 )
 
-
 fun main(args: Array<String>) {
     println("Hi! Select an option")
     println("1. Ask a question")
@@ -34,4 +38,66 @@ fun main(args: Array<String>) {
         "3" -> exit()
         else -> showError()
     }
+}
+
+fun showError() {
+    println("Oops! Option invalid")
+}
+
+fun exit() {
+    println("Bye!")
+}
+
+// Red function?      alt + enter
+
+fun show() {
+    println("Select an option")
+    println("1. Show all answer")
+    println("2. Get affirmative")
+    println("3. Get uncertain")
+    println("3. Get Negative")
+
+    val optionShow = readLine()
+    when(optionShow) {
+        "1" -> showByType()
+        "2" -> showByType(type = AFFIRMATIVE_ANSWER)
+        "3" -> showByType(type = NEGATIVE_ANSWER)
+        "4" -> showByType(type = DOUBTFUL_ANSWER)
+        else -> println("Oops! Closing...")
+    }
+}
+
+fun showByType(type : String = "ALL") {
+    when(type) {
+        // Foreach doesn't return
+        "ALL" -> answers.keys.forEach { answer ->
+            println(answer)
+        }
+
+        // Filter
+        // TODO: .also        fix
+        AFFIRMATIVE_ANSWER -> answers.filterValues {
+            values ->
+                values == AFFIRMATIVE_ANSWER
+        }.also { affirmativeAnswer ->
+            println(affirmativeAnswer.keys)
+        }
+
+        NEGATIVE_ANSWER -> answers.filterValues {
+                values -> values == NEGATIVE_ANSWER
+        }.also { negativeAnswer -> println(negativeAnswer.keys) }
+
+        DOUBTFUL_ANSWER -> answers.filterValues {
+                values -> values == DOUBTFUL_ANSWER
+        }.also { doubtfulAnswer -> println(doubtfulAnswer.keys) }
+
+    }
+}
+
+fun ask() {
+    println("Ask your question")
+    readLine()
+    println("answer is...")
+    val answer = answers.keys.random()
+    println(answer)
 }
