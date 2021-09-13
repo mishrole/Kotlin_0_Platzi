d8adc3dd0dce4cd19a1ed89a83786458fb368370
Mitchell Rodríguez <mitrodle@gmail.com>
2021-09-12 20:10:42 -0500

    add: Project

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,37 @@
+//Declarando valores de las respuestas
+const val AFFIRMATIVE_ANSWER = "✅"
+const val NEGATIVE_ANSWER = "❌"
+const val DOUBTFUL_ANSWER = "?"
+
+//uniendo las respuestas con sus valores
+val answers = mapOf(
+    "Yes" to AFFIRMATIVE_ANSWER,
+    "It's true" to AFFIRMATIVE_ANSWER,
+    "Totally" to AFFIRMATIVE_ANSWER,
+    "Undoubtedly" to AFFIRMATIVE_ANSWER,
+    "Ask in an other moment" to DOUBTFUL_ANSWER,
+    "I can't say you in this moment" to DOUBTFUL_ANSWER,
+    "Maybe yes, or maybe not" to DOUBTFUL_ANSWER,
+    "It's not going to happen" to NEGATIVE_ANSWER,
+    "Do not count on it" to NEGATIVE_ANSWER,
+    "Definitely not" to NEGATIVE_ANSWER,
+    "I don't believe it" to NEGATIVE_ANSWER
+)
+
+
 fun main(args: Array<String>) {
-    
+    println("Hi! Select an option")
+    println("1. Ask a question")
+    println("2. All answers")
+    println("3. Exit")
+
+    val input = readLine()
+
+    // When         String nullable
+    when(input) {
+        "1" -> ask()
+        "2" -> show()
+        "3" -> exit()
+        else -> showError()
+    }
 }
